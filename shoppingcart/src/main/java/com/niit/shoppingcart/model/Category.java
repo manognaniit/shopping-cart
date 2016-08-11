cc1a65d455f6127ef1c78c7bfaf8ebcdf43cb98b
manognaniit <vvrlmanogna@gmail.com>
2016-08-11 22:51:43 +0530

    Add files via upload
    shopping cart v-1

---
+++ shoppingcart/src/main/java/com/niit/shoppingcart/model/Category.java
@@ -0,0 +1,37 @@
+package com.niit.shoppingcart.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Category {
+	@Id
+ private String id;
+	@Column(name="name")
+	private String CategoryName;
+	private String description;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCategoryName() {
+		return CategoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		CategoryName = categoryName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
